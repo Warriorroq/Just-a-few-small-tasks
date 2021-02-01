d1dac9dedc8d5762934f2c14241402242385acc1
Warriorroq <Warriorroq@gmail.com>
2021-02-01 21:46:43 +0200

    Created module

---
+++ MyMathPy.py
@@ -0,0 +1,49 @@
+error = 0
+
+digits = []
+
+def add(a, b):
+    return a + b
+
+def mul(a, b):
+    return a * b
+
+def sub(a, b):
+    return a - b
+
+def div(a, b):
+    return a / b
+
+def eq(a, b):
+    return a == b
+
+def lt(a, b):
+    return a < b
+
+def nds(a, b):
+    if(a == 0 or b == 0):
+        return a + b
+
+    if(a > b):
+        return nds(a % b, b)
+    else:
+        return nds(a, b % a)
+
+def create(a, b):
+    try:
+        return float(a / b)
+    except:
+        return error
+
+def prt(a):
+    print(float(a))
+
+def inp():
+    c = input()
+    try:
+        return float(c)
+    except ValueError:
+        return inp()
+
+def clear_error():
+    error = 0
