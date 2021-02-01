80cc93060bb8d75c13d4216ac54b61f25ebddedf
Warriorroq <Warriorroq@gmail.com>
2021-02-01 21:23:25 +0200

    Added simple tasks

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 def add(a, b):
     return a + b
+
+def mul(a, b):
+    return a * b
+
+def sub(a, b):
+    return a - b
+
+def div(a, b):
+    return a / b
+
+def eq(a, b):
+    return a == b
+
+def lt(a, b):
+    return a < b
+
+#def nds(a, b):
+ #   if(a != 0 or b != 0):
+  #      return
+
+#print(nds(820,320))
