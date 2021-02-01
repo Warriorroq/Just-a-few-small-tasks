7ac857ea72a0b2fc354214e494f4b7a0c4f46084
Warriorroq <Warriorroq@gmail.com>
2021-02-01 22:16:06 +0200

    PyCharm clear

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import MyMathPy as math
-sum = 0.0
+import MyMathPy as MyMath
+s = 0.0
 
 for i in range(int(input())):
-    sum += math.add(math.inp(), math.inp())
+    s += MyMath.div(MyMath.inp(), MyMath.inp())
 
-print(" 1 / " + str(sum) + " = " + str(math.div(1, sum)))
+print(" 1 / " + str(s) +
+      " = " + str(MyMath.div(1, s)))
